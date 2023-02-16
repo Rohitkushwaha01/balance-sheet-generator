b02c6e89cd39e2bcf4691f8d70f7c88ae0715e8a
unknown <rohitkushwahac@gmail.com>
2023-02-16 18:25:50 +0530

    first file

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
-// Form-1  
-const sheetDate = document.getElementById('sheet-date');
-const cash = document.getElementById('cash');
-const accounts = document.getElementById('accounts');
-const inventory = document.getElementById('inventory');
-const expenses = document.getElementById('expenses');
-const notes = document.getElementById('notes');
+// Form-1
+const Businessname = document.getElementById("bname");
+const sheetDate = document.getElementById("sheet-date");
+const cash = document.getElementById("cash");
+const accounts = document.getElementById("accounts");
+const inventory = document.getElementById("inventory");
+const expenses = document.getElementById("expenses");
+const notes = document.getElementById("notes");
 
 // PDF-form
-const sheetDatePdf = document.getElementById('sheet-date-page-1')
-const cashPdf = document.getElementById('cash-data');
-const accPdf = document.getElementById('acc-data');
-const InvenPdf = document.getElementById('Inven-data');
-const prePaidPdf = document.getElementById('prepaid-data');
-const notesPdf = document.getElementById('notes-data');
-const totalCash = document.getElementById('total-cash');
+const cashPdf = document.getElementById("cash-data");
+const accPdf = document.getElementById("acc-data");
+const InvenPdf = document.getElementById("Inven-data");
+const prePaidPdf = document.getElementById("prepaid-data");
+const notesPdf = document.getElementById("notes-data");
+const otherPdf = document.getElementById("other-data");
+const totalCash = document.getElementById("total-cash");
+const businessPdf = document.getElementById("business-data");
 
-const arr = [sheetDate, cash, accounts, inventory, expenses, notes]
+const mmPdf = document.getElementById('mm-data');
+const ddPdf = document.getElementById('dd-data');
+const yyyyPdf = document.getElementById('yyyy-data');
 
-const arrPDF = [sheetDatePdf, cashPdf, accPdf, InvenPdf, prePaidPdf, notesPdf, totalCash];
+const datePdf = [mmPdf, ddPdf, yyyyPdf]
 
-arr.forEach((item, index)=>{
-    item.addEventListener('change', ()=>{
-        arrPDF[index].innerText = `$${item.value}`
+const arr = [cash, accounts, inventory, expenses, notes];
+
+const arrPDF = [
+  cashPdf,
+  accPdf,
+  InvenPdf,
+  prePaidPdf,
+  notesPdf,
+  otherPdf,
+  totalCash,
+];
+
+arr.forEach((item, index) => {
+  item.addEventListener("change", () => {
+    arrPDF[index].innerText = `$${item.value}`;
+  });
+});
+
+sheetDate.addEventListener("change", () => {
+    const date = new Date(sheetDate.value);
+    const mm = date.getMonth()+1;
+    const dd = date.getDate();
+    const yyyy = date.getFullYear();
+
+    const arrDate = [mm, dd, yyyy];
+
+    datePdf.forEach((item, index)=>{
+        console.log(item.innerText)
+        item.innerText = arrDate[index];
     })
-})
+});
 
-sheetDate.addEventListener('change', ()=>{
-    sheetDatePdf.value = `${sheetDate.value}`
-})
+Businessname.addEventListener("change", () => {
+  businessPdf.innerText = `${Businessname.value}`;
+});
+
 
